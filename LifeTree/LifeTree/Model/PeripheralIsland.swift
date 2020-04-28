839bb4ac79bf23a1fcd9f75ac34658acd7475102
Beatriz <bivilin@gmail.com>
2020-04-28 12:50:31 -0300

    Basic CoreData Structure for PeripheralIslands

--- LifeTree/LifeTree/Model/PeripheralIsland.swift
+++ LifeTree/LifeTree/Model/PeripheralIsland.swift
@@ -1,24 +1,27 @@
 //
-//  Action.swift
+//  PeriferalIsland.swift
 //  LifeTree
 //
 //  Created by Joyce Simão Clímaco on 24/04/20.
 //  Copyright © 2020 Beatriz Viseu Linhares. All rights reserved.
 //
 
-import CoreData
 import Foundation
+import CoreData
 
-class Action: NSManagedObject {
-    @NSManaged public var actionId: UUID?
+class PeripheralIsland: NSManagedObject {
+    @NSManaged public var islandId: UUID?
     @NSManaged public var name: String?
+    @NSManaged public var category: String?
+    @NSManaged public var healthStatus: NSNumber?
+    @NSManaged public var lastUpdate: Date?
 
     convenience init() {
         // get context
         let managedObjectContext: NSManagedObjectContext = CoreDataManager.sharedInstance.persistentContainer.viewContext
 
         // create entity description
-        let entityDescription = NSEntityDescription.entity(forEntityName: "Action", in: managedObjectContext)
+        let entityDescription = NSEntityDescription.entity(forEntityName: "PeripheralIsland", in: managedObjectContext)
 
         // call super
         self.init(entity: entityDescription!, insertInto: nil)
