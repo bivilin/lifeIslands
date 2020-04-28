10cc53039ae2813b38cd7d5fc9cc275726b58bad
Joyce <joyce.sclimaco@gmail.com>
2020-04-28 11:06:27 -0300

    Comenta o código e acessa cena do SpriteKit a partir do plano do SceneKit e do dicionário

--- LifeTree/LifeTree/Presentation/ViewControllers/IslandsSceneViewController.swift
+++ LifeTree/LifeTree/Presentation/ViewControllers/IslandsSceneViewController.swift
@@ -13,16 +13,22 @@ import SpriteKit
 class IslandsSceneViewController: UIViewController {
     
     @IBOutlet weak var islandsSCNView: SCNView!
+    
+    // MARK: Variable
+    
+    // Self Island variables
     var selfIslandSKScene: SKScene?
     var selfIsland: SelfIsland?
     
-    // Fica esquisito para 1, 2 e 4
-    // vamos ter que aumentar o raio para números altos
+    // Variables to distribute the periferal islands in an ellipse
     var numberOfIslands: Int = 6
-    var a: Double = 3.5
-    var b: Double = 2.5
-    var islandPlaneNodeArray: [SCNNode] = []
-    var dictionary: [String: SCNNode] = [:]
+    var a: Double = 3.5 // bigger semi-axis of the ellipse
+    var b: Double = 2.5 // smaller semi-axis of the ellipse
+    
+    // Dictionary with key being the id of the island, and value its corresponding SceneKit plane node
+    var islandDictionary: [String: SCNNode] = [:]
+    
+    // MARK: Lifecycle
 
     override func viewWillAppear(_ animated: Bool) {
         loadingData()
@@ -58,34 +64,52 @@ class IslandsSceneViewController: UIViewController {
         // Set the scene to the view
         self.islandsSCNView.scene = islandsSCNScene
         
+        // Corrects axis size based on the total number of islands
+        self.a += self.a * Double(self.numberOfIslands)/40
+        self.b += self.b * Double(self.numberOfIslands)/80
+        
+        // Angle of separation between the islands
         let separationAngle: Double = 2 * .pi / Double(self.numberOfIslands)
+        
+        // Position of ellipse focus
         let c = sqrt(self.a * self.a - self.b * self.b)
         
+        // Creates all islands
         for n in 1...self.numberOfIslands {
             
+            // Creates SceneKit plane
             var planeGeometry: SCNGeometry
             planeGeometry = SCNPlane(width: 1, height: 1)
             
+            // Assign a SpriteKit scene as texture to such plane
             if let planeMaterial = planeGeometry.firstMaterial {
                 
+                // Creates SpriteKit scene as an independent copy of the self island scene
                 let newSpriteKitScene = selfIslandSKScene!.copy() as! SKScene
-                planeMaterial.diffuse.contents = newSpriteKitScene
                 
+                // Asign scene as material
+                planeMaterial.diffuse.contents = newSpriteKitScene
+                planeMaterial.isDoubleSided = true
+                
+                // Changes label in island
                 let islandNode = newSpriteKitScene.children.first
                 let labelNode = islandNode?.childNode(withName: "nameLabelNode") as! SKLabelNode
                 labelNode.text = "Ilha" + String(n)
-                
-                planeMaterial.isDoubleSided = true
             }
             
+            // Add plane node to the SCNScene with all islands
             let islandNode = SCNNode(geometry: planeGeometry)
             islandsSCNScene.rootNode.addChildNode(islandNode)
             
-            self.dictionary[String(n)] = islandNode
+            // Append plane to the dictionary associating them with the island id
+            self.islandDictionary[String(n)] = islandNode
             
-            islandNode.eulerAngles.x = .pi - .pi/6
-            islandNode.position.y = 0
+            // Defines y position and tilt angle for the plane
+            islandNode.eulerAngles.x = .pi - .pi/6 // so the plane faces the camera
+            islandNode.position.y = 0 // define plane for the ellipse
             
+            // Positions the islando in the ellipse with focus in the self island
+            // Distinguishes between even and odd number of islands so they're better distributed in the ellipse
             if self.numberOfIslands % 2 == 0 {
                 islandNode.position.x = Float(self.b * sin((Double(n) + 1/2) * separationAngle))
                 islandNode.position.z = Float(self.a * cos((Double(n) + 1/2) * separationAngle) - (self.a-c))
@@ -95,51 +119,42 @@ class IslandsSceneViewController: UIViewController {
             }
         }
         
-        print(self.dictionary)
+        print(self.islandDictionary)
         
-//        var planeGeometry: SCNGeometry
-//        planeGeometry = SCNPlane(width: 1, height: 1)
-//
-//        if let planeMaterial = planeGeometry.firstMaterial {
-//
-//            let newSpriteKitScene = selfIslandSKScene!.copy() as! SKScene
-//            planeMaterial.diffuse.contents = newSpriteKitScene
-//
-//            let islandNode = newSpriteKitScene.children.first
-//            let labelNode = islandNode?.childNode(withName: "nameLabelNode") as! SKLabelNode
-//            labelNode.text = "CONSEGUI PORRA"
-//
-//            print(newSpriteKitScene.children.count)
-//
-//            planeMaterial.isDoubleSided = true
-//        }
+        // Acessa uma scene do SpriteKit a partir do node do plano do SceneKit
+        guard let nodeForIsland1: SCNNode = islandDictionary["1"] else {return}
+        guard let sceneForIsland1: SKScene = nodeForIsland1.geometry!.firstMaterial!.diffuse.contents as? SKScene else {return}
+        guard let nameIsland1 = sceneForIsland1.children.first?.childNode(withName: "nameLabelNode") as? SKLabelNode else {return}
+        nameIsland1.text = "Vai Corinthians"
         
-//        let islandNode = SCNNode(geometry: planeGeometry)
-//        islandNode.eulerAngles.x = .pi
-//
-//        islandsSCNScene.rootNode.addChildNode(islandNode)
-//        islandNode.position = SCNVector3(0, 1, 0)
+        // TODO
         
-        // Passos para criar as outras ilhas
-        // 1: Adicionar planos ao SceneKit programaticamente correspondendo às ilhas
-        // 2: Criar cena no SpriteKit programaticamente (ou podemos reaproveitar a mesma SKScene?)
-        // 3: Associar as duas
-        // 4: Conectar com as informações persistidas do CoreData
+        // Fazer cordas
+        
+        // Tirar sprite separado para a árvore
+        
+        // Calibrar as posições da câmera e das ilhas
+        // Fica esquisito para 1, 2 e 4 ilhas
 
-        // DICIONÁRIO: NOME DA ILHA (chave) -> SPRITEKIT
-        // Fazer array de planos, cada um com uma textura do SpriteKit
-        // SpriteKit -> podemos fazer o sks herdar de uma classe -
-        // Fazer uma SKScene para cada ilha, já que teremos um número máximo
+        // Conectar com o CoreData (quando buscamos as ilhas no CoreData, elas sempre serão devolvidas por ordem de id?) Se sim, toda vez que recriarmos o mundo quando a pessoa entrar no app, ele vai ser o mesmo!
+        // Estabelecer como será a navegação
+        
+        // O dicionário é persistido quando vamos para outra página e voltamos?
+        // Verificar se mesmo indo para outra tela a minha scene continua existindo
+        // Como ela vai se comportar com segues que vão para ela? Talvez tenhamos que chamar funções diferentes dependendo de quando a pessoa acessa a Scene (ao entrar no app ou voltando de outra tela). Se for voltando de outra tela e a Scene não tiver persistido na troca, devemos persistir pelo menos a última posição da câmera para ter continuidade
         
         // CÂMERA
+        
         // Mover a câmera com pinch e pan
         // Limitar o movimento dessa câmera com constraints
         // Escrever uma função que dependendo da posição da câmera mostra um card específico
         // OU
         // Cordinhas: joint (articulação) -> colocar efeitos de física
         // Colocar um corpo de física maior que a corda e transparente para a pessoa tocar
-        
     }
+    
+    // MARK: Actions
+    
     @IBAction func criarIlha(_ sender: Any) {
         let island = SelfIsland()
         island.name = "Mundo Mágico de Oz"
@@ -175,6 +190,8 @@ class IslandsSceneViewController: UIViewController {
             }
         }
     }
+    
+    // MARK: Helpers
 
     func setUpCameraControl(sceneView: SCNView) {
         // Allows the user to manipulate the camera
