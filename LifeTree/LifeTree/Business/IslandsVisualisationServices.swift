8c7c458c2e9688a553e56209a1e5c53a875f3e4d
Joyce <joyce.sclimaco@gmail.com>
2020-04-28 13:42:20 -0300

    Separa classe para criação de ilhas

--- LifeTree/LifeTree/Business/IslandsVisualisationServices.swift
+++ LifeTree/LifeTree/Business/IslandsVisualisationServices.swift
@@ -10,16 +10,27 @@ import Foundation
 import SceneKit
 import SpriteKit
 
-class IslandsVisualisation {
+class IslandsVisualisationServices {
     
     // Axis to distribute the periferal islands in an ellipse
     var a: Double = 3.5 // bigger semi-axis of the ellipse
     var b: Double = 2.5 // smaller semi-axis of the ellipse
-    let planeLength: Double = 1
+    let planeLength: CGFloat = 1
+    var islandDictionary: [String: SCNNode] = [:]
     
+    // Add self island to scene
+    func addSelfIslandToScene(islandsSCNScene: SCNScene) {
+        // Set SpriteKit scene as the material for the SceneKit plane
+        if let selfIslandPlane = islandsSCNScene.rootNode.childNode(withName: "selfIslandPlane", recursively: true),
+            let selfIslandPlaneGeometry = selfIslandPlane.geometry {
+            self.setPlaneMaterialAsIslandSKScene(planeGeometry: selfIslandPlaneGeometry)
+        }
+    }
+    
+    // Add all periferal islands to scene
     func addPeriferalIslandsToScene(islandsSCNScene: SCNScene, numberOfIslands: Int) {
         
-        self.axisSizeCorrection(numberOfIslands: numberOfIslands)
+        axisSizeCorrection(numberOfIslands: numberOfIslands)
         // Position of ellipse focus
         let ellipseFocalLength: Double = sqrt(self.a * self.a - self.b * self.b)
         
@@ -27,29 +38,40 @@ class IslandsVisualisation {
         let separationAngle: Double = 2 * .pi / Double(numberOfIslands)
         
         // Creates all islands
-        for n in 1...self.numberOfIslands {
-            self.addPeriferalIslandsToScene(islandsSCNScene: islandsSCNScene, n: n, ellipseFocalLength: ellipseFocalLength, numberOfIslands: numberOfIslands, separationAngle: separationAngle)
+        for n in 1...numberOfIslands {
+            addIslandToSCNScene(islandsSCNScene: islandsSCNScene, n: n, ellipseFocalLength: ellipseFocalLength, numberOfIslands: numberOfIslands, separationAngle: separationAngle)
+        }
+        print(self.islandDictionary)
     }
     
+    // Add a single periferal island with index n to the scene
     func addIslandToSCNScene(islandsSCNScene: SCNScene, n: Int, ellipseFocalLength: Double, numberOfIslands: Int, separationAngle: Double) {
-        
+
         // Creates plane with island
-        var planeGeometry: SCNGeometry = self.createPlaneGeometryWithIslandSKScene()
+        var planeGeometry: SCNGeometry
+        planeGeometry = SCNPlane(width: self.planeLength, height: self.planeLength)
+        self.setPlaneMaterialAsIslandSKScene(planeGeometry: planeGeometry)
         
         // Add plane node to the SCNScene with all islands
         let islandNode = SCNNode(geometry: planeGeometry)
         islandsSCNScene.rootNode.addChildNode(islandNode)
         
         // Append plane to the dictionary associating them with the island id
-        // self.islandDictionary[String(n)] = islandNode
+        self.islandDictionary[String(n)] = islandNode
         
         // Defines y position and tilt angle for the plane
         islandNode.eulerAngles.x = .pi - .pi/6 // so the plane faces the camera
-        islandNode.position.y = 0 // define plane for the ellipse
         
-        // Positions the islando in the ellipse with focus in the self island
+        self.positionIslandInEllipse(islandNode: islandNode, numberOfIslands: numberOfIslands, n: n, separationAngle: separationAngle, ellipseFocalLength: ellipseFocalLength)
+    }
+    
+    // Position a periferal islands in the ellipse with focus in the self island
+    func positionIslandInEllipse(islandNode: SCNNode, numberOfIslands: Int, n: Int, separationAngle: Double, ellipseFocalLength: Double) {
+        
+        // Define plane for the ellipse
+        islandNode.position.y = 0
         // Distinguishes between even and odd number of islands so they're better distributed in the ellipse
-        if self.numberOfIslands % 2 == 0 {
+        if numberOfIslands % 2 == 0 {
             islandNode.position.x = Float(self.b * sin((Double(n) + 1/2) * separationAngle))
             islandNode.position.z = Float(self.a * cos((Double(n) + 1/2) * separationAngle) - (self.a - ellipseFocalLength))
         } else {
@@ -60,15 +82,12 @@ class IslandsVisualisation {
     
     func axisSizeCorrection(numberOfIslands: Int) {
         // Corrects axis size based on the total number of islands
-        self.a += self.a * Double(self.numberOfIslands)/40
-        self.b += self.b * Double(self.numberOfIslands)/80
+        self.a += self.a * Double(numberOfIslands)/40
+        self.b += self.b * Double(numberOfIslands)/80
     }
     
-    func createPlaneGeometryWithIslandSKScene() -> SCNGeometry {
-        // Create plane
-        var planeGeometry: SCNGeometry
-        planeGeometry = SCNPlane(width: self.planeLength, height: self.planeLength)
-        
+    // Define the material for a plane as the island SpriteKit scene model
+    func setPlaneMaterialAsIslandSKScene(planeGeometry: SCNGeometry) {
         // Assign a SpriteKit scene as texture to such plane
         if let planeMaterial = planeGeometry.firstMaterial {
             
@@ -76,17 +95,19 @@ class IslandsVisualisation {
             let newSpriteKitScene = SKScene(fileNamed: "IslandSpriteScene.sks")!.copy() as! SKScene
             // Asign scene as material
             planeMaterial.diffuse.contents = newSpriteKitScene
+            // Makes the material double sided, otherwise the plane can only be seen when the camera is pointed at its +z direction (which is not our case)
             planeMaterial.isDoubleSided = true
         }
-        return planeGeometry
     }
     
-    func changePeriferalIslandLabel(islandId: String, text: String) {
+    // Changes label of a periferal island
+    func changePeriferalIslandLabel(islandId: String, text: String, islandDictionary: [String: SCNNode]) {
         // Acessa uma scene do SpriteKit a partir do node do plano do SceneKit
         guard let nodeForIsland: SCNNode = islandDictionary[islandId] else {return}
         guard let sceneForIsland: SKScene = nodeForIsland.geometry!.firstMaterial!.diffuse.contents as? SKScene else {return}
         guard let nameIsland = sceneForIsland.children.first?.childNode(withName: "nameLabelNode") as? SKLabelNode else {return}
-        nameIsland1.text = text
+        nameIsland.text = text
     }
     
 }
+
--- LifeTree/LifeTree/Presentation/ViewControllers/IslandsSceneViewController.swift
+++ LifeTree/LifeTree/Presentation/ViewControllers/IslandsSceneViewController.swift
@@ -14,17 +14,15 @@ class IslandsSceneViewController: UIViewController {
     
     @IBOutlet weak var islandsSCNView: SCNView!
     
-    // MARK: Variable
+    // MARK: Variables
+    
+    // COLOCAR INICIALIZADOR NESSA CLASSE!
     
     // Self Island variables
     var selfIslandSKScene: SKScene?
     var selfIsland: SelfIsland?
     
-    // Variables to distribute the periferal islands in an ellipse
-    var numberOfIslands: Int = 6
-    var a: Double = 3.5 // bigger semi-axis of the ellipse
-    var b: Double = 2.5 // smaller semi-axis of the ellipse
-    
+    let islandsVisualizationServices = IslandsVisualisationServices()
     // Dictionary with key being the id of the island, and value its corresponding SceneKit plane node
     var islandDictionary: [String: SCNNode] = [:]
     
@@ -49,108 +47,16 @@ class IslandsSceneViewController: UIViewController {
         // SceneKit camera
         // let cameraNode = islandsSCNScene.rootNode.childNode(withName: "camera", recursively: true)
         
-        // Set SpriteKit scene as the material for the SceneKit plane
-        if let selfIslandPlane = islandsSCNScene.rootNode.childNode(withName: "selfIslandPlane", recursively: true),
-            let geometry = selfIslandPlane.geometry,
-            let material = geometry.firstMaterial {
-            
-            // Rotates the plane reference frame by pi (180 degrees), otherwize the SpriteKit Scene appears mirroed
-            material.diffuse.contents = selfIslandSKScene
-            // Makes the material double sided, otherwise the plane can only be seen when the camera is pointed at its +z direction
-            // In our case, the camera is pointed to the plane -z direction, so if not double sided, the plane won't even be visible
-            material.isDoubleSided = true
-        }
+        // Add self islando do scene
+        self.islandsVisualizationServices.addSelfIslandToScene(islandsSCNScene: islandsSCNScene)
+        
+        // Add periferal islands to scene
+        self.islandsVisualizationServices.addPeriferalIslandsToScene(islandsSCNScene: islandsSCNScene, numberOfIslands: 6)
         
         // Set the scene to the view
         self.islandsSCNView.scene = islandsSCNScene
         
-        // Corrects axis size based on the total number of islands
-        self.a += self.a * Double(self.numberOfIslands)/40
-        self.b += self.b * Double(self.numberOfIslands)/80
-        
-        // Angle of separation between the islands
-        let separationAngle: Double = 2 * .pi / Double(self.numberOfIslands)
-        
-        // Position of ellipse focus
-        let c = sqrt(self.a * self.a - self.b * self.b)
-        
-        // Creates all islands
-        for n in 1...self.numberOfIslands {
-            
-            // Creates SceneKit plane
-            var planeGeometry: SCNGeometry
-            planeGeometry = SCNPlane(width: 1, height: 1)
-            
-            // Assign a SpriteKit scene as texture to such plane
-            if let planeMaterial = planeGeometry.firstMaterial {
-                
-                // Creates SpriteKit scene as an independent copy of the self island scene
-                let newSpriteKitScene = selfIslandSKScene!.copy() as! SKScene
-                
-                // Asign scene as material
-                planeMaterial.diffuse.contents = newSpriteKitScene
-                planeMaterial.isDoubleSided = true
-                
-                // Changes label in island
-                let islandNode = newSpriteKitScene.children.first
-                let labelNode = islandNode?.childNode(withName: "nameLabelNode") as! SKLabelNode
-                labelNode.text = "Ilha" + String(n)
-            }
-            
-            // Add plane node to the SCNScene with all islands
-            let islandNode = SCNNode(geometry: planeGeometry)
-            islandsSCNScene.rootNode.addChildNode(islandNode)
-            
-            // Append plane to the dictionary associating them with the island id
-            self.islandDictionary[String(n)] = islandNode
-            
-            // Defines y position and tilt angle for the plane
-            islandNode.eulerAngles.x = .pi - .pi/6 // so the plane faces the camera
-            islandNode.position.y = 0 // define plane for the ellipse
-            
-            // Positions the islando in the ellipse with focus in the self island
-            // Distinguishes between even and odd number of islands so they're better distributed in the ellipse
-            if self.numberOfIslands % 2 == 0 {
-                islandNode.position.x = Float(self.b * sin((Double(n) + 1/2) * separationAngle))
-                islandNode.position.z = Float(self.a * cos((Double(n) + 1/2) * separationAngle) - (self.a-c))
-            } else {
-                islandNode.position.x = Float(self.b * sin(Double(n) * separationAngle))
-                islandNode.position.z = Float(self.a * cos(Double(n) * separationAngle) - (self.a-c))
-            }
-        }
-        
         print(self.islandDictionary)
-        
-        // Acessa uma scene do SpriteKit a partir do node do plano do SceneKit
-        guard let nodeForIsland1: SCNNode = islandDictionary["1"] else {return}
-        guard let sceneForIsland1: SKScene = nodeForIsland1.geometry!.firstMaterial!.diffuse.contents as? SKScene else {return}
-        guard let nameIsland1 = sceneForIsland1.children.first?.childNode(withName: "nameLabelNode") as? SKLabelNode else {return}
-        nameIsland1.text = "Vai Corinthians"
-        
-        // TODO
-        
-        // Fazer cordas
-        
-        // Tirar sprite separado para a árvore
-        
-        // Calibrar as posições da câmera e das ilhas
-        // Fica esquisito para 1, 2 e 4 ilhas
-
-        // Conectar com o CoreData (quando buscamos as ilhas no CoreData, elas sempre serão devolvidas por ordem de id?) Se sim, toda vez que recriarmos o mundo quando a pessoa entrar no app, ele vai ser o mesmo!
-        // Estabelecer como será a navegação
-        
-        // O dicionário é persistido quando vamos para outra página e voltamos?
-        // Verificar se mesmo indo para outra tela a minha scene continua existindo
-        // Como ela vai se comportar com segues que vão para ela? Talvez tenhamos que chamar funções diferentes dependendo de quando a pessoa acessa a Scene (ao entrar no app ou voltando de outra tela). Se for voltando de outra tela e a Scene não tiver persistido na troca, devemos persistir pelo menos a última posição da câmera para ter continuidade
-        
-        // CÂMERA
-        
-        // Mover a câmera com pinch e pan
-        // Limitar o movimento dessa câmera com constraints
-        // Escrever uma função que dependendo da posição da câmera mostra um card específico
-        // OU
-        // Cordinhas: joint (articulação) -> colocar efeitos de física
-        // Colocar um corpo de física maior que a corda e transparente para a pessoa tocar
     }
     
     // MARK: Actions
