478dbd45bf3e98b65f44f71100057d4090f86c36
Victoria Andressa Faria <victoriaamdf@gmail.com>
2020-04-29 02:29:55 -0300

    Add Card on selfIsland

---
+++ LifeTree/LifeTree/Presentation/ViewControllers/UI/FloatingPanelViewController.swift
@@ -0,0 +1,102 @@
+//
+//  FloatingPanelViewController.swift
+//  LifeTree
+//
+//  Created by Victoria Andressa S. M. Faria on 28/04/20.
+//  Copyright © 2020 Beatriz Viseu Linhares. All rights reserved.
+//
+
+import UIKit
+import FloatingPanel
+
+class FloatingPanelViewController: UIViewController, FloatingPanelControllerDelegate {
+
+    var initialColor: UIColor = .white
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+// MARK: FloatingPanelControllerDelegate
+    
+    func floatingPanel(_ vc: FloatingPanelController, layoutFor newCollection: UITraitCollection) -> FloatingPanelLayout? {
+        return FloatingPanelCardLayout()
+    }
+
+    func floatingPanel(_ vc: FloatingPanelController, behaviorFor newCollection: UITraitCollection) -> FloatingPanelBehavior? {
+        return FloatingPanelCardBehavior()
+    }
+
+}
+
+class CardViewController: UIViewController {
+    // esse é o controller do card
+    // colocar o que quisermos referente ao card
+    // sar a mesma classe mas com atributos diferentes para ilhas diferentes
+}
+
+// MARK: My custom layout
+
+class FloatingPanelCardLayout: FloatingPanelLayout {
+    
+    public var initialPosition: FloatingPanelPosition {
+        return .tip
+    }
+    
+    public func insetFor(position: FloatingPanelPosition) -> CGFloat? {
+        switch position {
+        case .full: return 16.0 // A top inset from safe area
+        case .half: return 262.0 // A bottom inset from the safe area
+        case .tip: return 85.0 + 44.0 // A bottom inset from the safe area Visible + ToolView
+        default: return nil // Or `case .hidden: return nil`
+        }
+    }
+
+    func backdropAlphaFor(position: FloatingPanelPosition) -> CGFloat {
+        return 0.9
+    }
+}
+
+// MARK: Custom behavior
+
+class FloatingPanelCardBehavior: FloatingPanelBehavior {
+    
+    var velocityThreshold: CGFloat {
+        return 15.0
+    }
+
+    func interactionAnimator(_ fpc: FloatingPanelController, to targetPosition: FloatingPanelPosition, with velocity: CGVector) -> UIViewPropertyAnimator {
+        let timing = timeingCurve(to: targetPosition, with: velocity)
+        return UIViewPropertyAnimator(duration: 0, timingParameters: timing)
+    }
+
+    private func timeingCurve(to: FloatingPanelPosition, with velocity: CGVector) -> UITimingCurveProvider {
+        let damping = self.damping(with: velocity)
+        return UISpringTimingParameters(dampingRatio: damping,
+                                        frequencyResponse: 0.4,
+                                        initialVelocity: velocity)
+    }
+
+    private func damping(with velocity: CGVector) -> CGFloat {
+        switch velocity.dy {
+        case ...(-velocityThreshold):
+            return 0.7
+        case velocityThreshold...:
+            return 0.7
+        default:
+            return 1.0
+        }
+    }
+}
