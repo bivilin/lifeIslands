ea1f7dba9efcfa312cd58c6d2d8d638d97da950d
Joyce <joyce.sclimaco@gmail.com>
2020-04-29 12:09:25 -0300

    Remove comentário desnecessário da minha reunião com o Guga

--- LifeTree/LifeTree/Presentation/ViewControllers/IslandsViewController.swift
+++ LifeTree/LifeTree/Presentation/ViewControllers/IslandsViewController.swift
@@ -51,25 +51,6 @@ class IslandsViewController: UIViewController, FloatingPanelControllerDelegate{
         setupFloatingPanel()
         cardView = storyboard?.instantiateViewController(withIdentifier: "Card") as? CardViewController
         showFloatingPanel()
-
-        // Passos para criar as outras ilhas
-        // 1: Adicionar planos ao SceneKit programaticamente correspondendo às ilhas
-        // 2: Criar cena no SpriteKit programaticamente (ou podemos reaproveitar a mesma SKScene?)
-        // 3: Associar as duas
-        // 4: Conectar com as informações persistidas do CoreData
-
-        // DICIONÁRIO: NOME DA ILHA (chave) -> SPRITEKIT
-        // Fazer array de planos, cada um com uma textura do SpriteKit
-        // SpriteKit -> podemos fazer o sks herdar de uma classe -
-        // Fazer uma SKScene para cada ilha, já que teremos um número máximo
-        
-        // CÂMERA
-        // Mover a câmera com pinch e pan
-        // Limitar o movimento dessa câmera com constraints
-        // Escrever uma função que dependendo da posição da câmera mostra um card específico
-        // OU
-        // Cordinhas: joint (articulação) -> colocar efeitos de física
-        // Colocar um corpo de física maior que a corda e transparente para a pessoa tocar
         
         // SceneKit camera
         // let cameraNode = islandsSCNScene.rootNode.childNode(withName: "camera", recursively: true)
