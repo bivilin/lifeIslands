46d66e1c4c6047518d9a132a090d4dbbe5d831f9
Joyce <joyce.sclimaco@gmail.com>
2020-05-07 17:57:43 -0300

    Faz movimento da câmera programaticamente com as devidas restrições

---
+++ LifeTree/LifeTree/Business/VectorServices.swift
@@ -0,0 +1,9 @@
+//
+//  vectorServices.swift
+//  LifeTree
+//
+//  Created by Joyce Simão Clímaco on 07/05/20.
+//  Copyright © 2020 Beatriz Viseu Linhares. All rights reserved.
+//
+
+import Foundation
