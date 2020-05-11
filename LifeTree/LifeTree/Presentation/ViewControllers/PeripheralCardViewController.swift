060d34b1436c1490e23cc89ba72b7cc7ce2c41d9
Beatriz <bivilin@gmail.com>
2020-05-11 16:16:12 -0300

    Primeiros testes com a tableview de ações

--- LifeTree/LifeTree/Presentation/ViewControllers/PeripheralCardViewController.swift
+++ LifeTree/LifeTree/Presentation/ViewControllers/PeripheralCardViewController.swift
@@ -13,8 +13,11 @@ class PeripheralCardViewController: UIViewController {
 
     @IBOutlet weak var nameIsland: UILabel!
     @IBOutlet weak var phrase: UILabel!
+    @IBOutlet weak var actionsTableView: UITableView!
 
     var peripheralIsland: PeripheralIsland?
+    var islandActions: [Actions] = []
+    var numberOfActions: Int = 0
 
     override func viewWillAppear(_ animated: Bool) {
         nameIsland.text = peripheralIsland?.name
@@ -24,5 +27,39 @@ class PeripheralCardViewController: UIViewController {
         } else {
             phrase.text = "Sua saúde ainda não foi definida"
         }
+
+        // Table View
+        islandActions = [Actions(name: "Regar as plantas"), Actions(name: "Varrer a calçada"), Actions(name: "Lavar a roupa")]
+        numberOfActions = islandActions.count
+
+        actionsTableView.delegate = self
+        actionsTableView.dataSource = self
+        actionsTableView.reloadData()
+
+
+    }
+
+
+}
+
+extension PeripheralCardViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return numberOfActions
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = actionsTableView.dequeueReusableCell(withIdentifier: "actionCell", for: indexPath)
+        cell.textLabel?.text = islandActions[indexPath.row].name
+        return cell
+    }
+
+
+}
+
+class Actions {
+    var name: String
+
+    init(name: String) {
+        self.name = name
     }
 }
