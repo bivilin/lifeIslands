dfd6fb81b81d40a9b1e264480f5aac040dd3c68c
Beatriz <bivilin@gmail.com>
2020-05-11 15:28:55 -0300

    Botões de debug. Métodos de exemplo para serem utilizados pelo front-end.

--- LifeTree/LifeTree/Presentation/ViewControllers/PeripheralCardViewController.swift
+++ LifeTree/LifeTree/Presentation/ViewControllers/PeripheralCardViewController.swift
@@ -25,4 +25,49 @@ class PeripheralCardViewController: UIViewController {
             phrase.text = "Sua saúde ainda não foi definida"
         }
     }
+
+
+    // MARK: Debug Buttons
+
+    @IBAction func newAction(_ sender: Any) {
+        let action = Action()
+
+        action.actionId = UUID()
+        action.name = "Ação teste"
+        action.impactLevel = 50
+
+        if let relatedIsland = self.peripheralIsland {
+            ActionDataServices.createAction(action: action, relatedIsland: relatedIsland) { (error) in
+                if error != nil {
+                    print(error.debugDescription)
+                } else {
+                    print("Ação criada com sucesso.")
+                }
+            }
+        }
+    }
+
+    @IBAction func checkIslandActions(_ sender: Any) {
+        ActionDataServices.getIslandActions(island: peripheralIsland!) { (error, actions) in
+            if (error != nil) {
+                print(error.debugDescription)
+            } else {
+                if let islandActions = actions {
+                    print("Há \(islandActions.count) ações na ilha \(self.peripheralIsland!.name!).")
+                    for action in islandActions {
+                        print("Ação \(action.name!) - Nível de impacto: \(action.impactLevel!)")
+                    }
+                }
+            }
+        }
+    }
+
+    // MARK: Table View - List of Actions
+    // Deve ser populada utilizando o método
+    // ActionDataServices.getIslandActions() conforme
+    // exemplificado no botão de debug
+
+    // MARK: Add New Action Button
+    // Deve levar o usuário a uma nova tela (CreateActionViewController)
+    // Informações que devem ser passadas: PeripheralIsland
 }
