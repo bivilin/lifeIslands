4c0666084d512e66cef7f8646d5e517723f2fead
Beatriz <bivilin@gmail.com>
2020-05-28 09:28:21 -0300

    Pequeno ajuste da inclusão dos dados no banco

--- LifeTree/LifeTree/Presentation/ViewControllers/CreateActionViewController.swift
+++ LifeTree/LifeTree/Presentation/ViewControllers/CreateActionViewController.swift
@@ -138,7 +138,7 @@ class CreateActionViewController: UIViewController {
         action.actionId = UUID()
         action.name = actionNameTextField.text
         action.impactLevel = NSNumber(value: impactLevelSlider.value)
-        action.impactReason = impactReasonTextField.text
+        action.impactReason = impactReasonTextView.text
 
         if let relatedIsland = self.island {
             // Persiste ação no banco de dados
