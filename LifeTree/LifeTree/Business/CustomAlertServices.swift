2463448791eabacdc20cf26974963a572d825973
Joyce <joyce.sclimaco@gmail.com>
2020-05-28 11:12:57 -0300

    Cria classe de alertas e evita que o fundo da célula fixa da tableView da ilha periférica fique cinza

---
+++ LifeTree/LifeTree/Business/CustomAlertServices.swift
@@ -0,0 +1,9 @@
+//
+//  CustomAlertServices.swift
+//  LifeTree
+//
+//  Created by Joyce Simão Clímaco on 28/05/20.
+//  Copyright © 2020 Beatriz Viseu Linhares. All rights reserved.
+//
+
+import Foundation
---
+++ LifeTree/LifeTree/Presentation/ViewControllers/CustomAlertViewController.swift
@@ -0,0 +1,8 @@
+//
+//  CustomAlertViewController.swift
+//  
+//
+//  Created by Joyce Simão Clímaco on 28/05/20.
+//
+
+import Foundation
