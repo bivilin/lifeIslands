0f37e7edad1df6002fe8d42b5371a887d0e2f409
Joyce <joyce.sclimaco@gmail.com>
2020-05-29 09:49:34 -0300

    Corrige arquivo corrompido

--- LifeTree/LifeTree/Presentation/ViewControllers/CustomAlertViewController.swift
+++ LifeTree/LifeTree/Presentation/ViewControllers/CustomAlertViewController.swift
@@ -1,8 +1,9 @@
 //
 //  CustomAlertViewController.swift
-//  
+//  LifeTree
 //
-//  Created by Joyce Simão Clímaco on 28/05/20.
+//  Created by Joyce Simão Clímaco on 29/05/20.
+//  Copyright © 2020 Beatriz Viseu Linhares. All rights reserved.
 //
 
 import Foundation
@@ -13,47 +14,47 @@ protocol CustomAlertViewDelegate {
 }
 
 class CustomAlertViewController: UIViewController {
-    
+
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var bottomButton: UIButton!
     @IBOutlet weak var dismissButton: UIButton!
     @IBOutlet weak var alertDetailsView: UIView!
-    
+
     var alertTitle: String = ""
     var alertDescription: String = ""
     var hasBottomButton: Bool = false
     var hasDismissButton: Bool = true
-    
+
     var delegate: CustomAlertViewDelegate?
-        
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Set up background to mimic the iOS native Alert
         self.view.backgroundColor = UIColor.black.withAlphaComponent(0.6)
-        
+
         // Update labels
         self.titleLabel.text = self.alertTitle
         self.descriptionLabel.text = self.alertDescription
-        
+
         // Set up cornerRadius
         self.dismissButton.layer.cornerRadius = self.dismissButton.frame.width/2
         let cornerRadius: CGFloat = 15
         self.bottomButton.layer.cornerRadius = cornerRadius
         self.alertDetailsView.layer.cornerRadius = cornerRadius
-        
+
         // Hide or show bottom buttons
         self.bottomButton.isHidden = !self.hasBottomButton
         self.dismissButton.isHidden = !self.hasDismissButton
     }
-    
-    @IBAction func bottomButtonAction(_ sender: Any) {
+
+    @IBAction func bottomButtonAction( sender: Any) {
         self.delegate?.bottomButtonAction()
     }
-    
-    @IBAction func dismissButtonAction(_ sender: Any) {
+
+    @IBAction func dismissButtonAction( sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
-    
+
 }
