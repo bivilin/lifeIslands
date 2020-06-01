60a971645a817c5b231e08cceeb05afccbda719f
Joyce <joyce.sclimaco@gmail.com>
2020-06-01 13:02:48 -0300

    Front-end do xib da page

---
+++ LifeTree/LifeTree/Presentation/ViewControllers/WalkthroughViewController.swift
@@ -0,0 +1,15 @@
+//
+//  WalkthroughViewController.swift
+//  LifeTree
+//
+//  Created by Joyce Simão Clímaco on 01/06/20.
+//  Copyright © 2020 Beatriz Viseu Linhares. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class WalkthroughViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+}
