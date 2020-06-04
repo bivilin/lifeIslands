ab2b2b9783218a136c1626c80f3a132c36a26a18
Beatriz <bivilin@gmail.com>
2020-06-04 16:20:10 -0300

    Melhorando visualização (espaçamento, cores, fontes)

--- LifeTree/LifeTree/Presentation/Cells/LifeAreaTableViewCell.swift
+++ LifeTree/LifeTree/Presentation/Cells/LifeAreaTableViewCell.swift
@@ -13,6 +13,7 @@ class LifeAreaTableViewCell: UITableViewCell {
 
     @IBOutlet weak var contourView: UIView!
     @IBOutlet weak var islandTextField: UITextField!
+    @IBOutlet weak var lifeAreaLabel: UILabel!
 
 
     override func awakeFromNib() {
@@ -25,11 +26,14 @@ class LifeAreaTableViewCell: UITableViewCell {
         self.contourView.layer.shadowColor = UIColor.black.cgColor
         self.contourView.layer.shadowOpacity = 0.10
         self.contourView.layer.shadowRadius = 4
+
+        self.lifeAreaLabel.textColor = UIColor(red: 0.31, green: 0.31, blue: 0.31, alpha: 1)
         self.selectionStyle = .none
     }
 
     // Método para popular conteúdos da célula (texto e imagem)
     func loadContents(islandName: String) {
-        self.islandTextField.placeholder = islandName
+        //self.islandTextField.placeholder = islandName
+        self.lifeAreaLabel.text = islandName
     }
 }
