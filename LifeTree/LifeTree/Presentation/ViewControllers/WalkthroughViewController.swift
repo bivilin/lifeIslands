4174601f9cb7a80a7130e7d9b4a71f35d06e97a1
Joyce <joyce.sclimaco@gmail.com>
2020-06-05 10:59:37 -0300

    Faz o walkthrough igual a nova versão do Figma

--- LifeTree/LifeTree/Presentation/ViewControllers/WalkthroughViewController.swift
+++ LifeTree/LifeTree/Presentation/ViewControllers/WalkthroughViewController.swift
@@ -49,32 +49,32 @@ class WalkthroughViewController: UIViewController {
     func createPages() -> [WalkthroughPage] {
         let page1: WalkthroughPage = Bundle.main.loadNibNamed("WalkthroughPageView", owner: self, options: nil)?.first as! WalkthroughPage
         page1.imageView.image = UIImage(named: "Asset1_onboarding")
-        page1.titleLabel.text = "Separe as áreas de sua vida"
-        page1.descriptionLabel.text = "Organize áreas da sua vida separando-as em ilhas para dar a devida atenção a cada uma individualmente."
+        page1.titleLabel.text = "Visualize as áreas da sua vida"
+        page1.descriptionLabel.text = "Você terá uma ilha para área da sua vida e conseguirá navegar entre elas arrastando a tela vertical e horizontalmente"
         page1.startButton.isHidden = true
         
         let page2: WalkthroughPage = Bundle.main.loadNibNamed("WalkthroughPageView", owner: self, options: nil)?.first as! WalkthroughPage
-        page2.imageView.image = UIImage(named: "Asset2_onboarding")
+        page2.imageView.image = UIImage(named: "Asset6_onboarding")
         page2.titleLabel.text = "Adicione atividades para cuidar da sua ilha"
-        page2.descriptionLabel.text = "Suas atividades servirão de parâmetro para acompanhar seu progresso e de estímulo para cuidar das áreas sua vida."
+        page2.descriptionLabel.text = "Suas atividades servirão de parâmetro para acompanhar seu progresso e de estímulo para cuidar das áreas da sua vida."
         page2.startButton.isHidden = true
         
         let page3: WalkthroughPage = Bundle.main.loadNibNamed("WalkthroughPageView", owner: self, options: nil)?.first as! WalkthroughPage
-        page3.imageView.image = UIImage(named: "Asset3_onboarding")
+        page3.imageView.image = UIImage(named: "Asset7_onboarding")
         page3.titleLabel.text = "Ganhe recompensas e cultive sua ilha"
         page3.descriptionLabel.text = "Ao realizar tarefas, você ganhará gotas para regar sua ilha. Quanto mais você regar, mais saudável ela ficará."
         page3.startButton.isHidden = true
         
         let page4: WalkthroughPage = Bundle.main.loadNibNamed("WalkthroughPageView", owner: self, options: nil)?.first as! WalkthroughPage
-        page4.imageView.image = UIImage(named: "Asset4_onboarding")
+        page4.imageView.image = UIImage(named: "Asset8_onboarding")
         page4.titleLabel.text = "Você está no centro"
-        page4.descriptionLabel.text = "A ilha central representa VOCÊ e é afetada pela saúde de todas as outras ilhas. Portanto, acompanhe a saúde da sua ilha"
+        page4.descriptionLabel.text = "A ilha central representa VOCÊ, e a saúde dela é o reflexo de todas as outras ilhas. Portanto, não esqueça nenhuma delas."
         page4.startButton.isHidden = true
         
         let page5: WalkthroughPage = Bundle.main.loadNibNamed("WalkthroughPageView", owner: self, options: nil)?.first as! WalkthroughPage
-        page5.imageView.image = UIImage(named: "Asset5_onboarding")
+        page5.imageView.image = UIImage(named: "Asset2_onboarding")
         page5.titleLabel.text = "As estações são seu guia"
-        page5.descriptionLabel.text = "Visualize facilmente todas as áreas da sua vida e quão saudáveis estão. Se alguma precisar de mais atenção, você saberá."
+        page5.descriptionLabel.text = "Acompanhe a saúde de cada ilha e saiba qual priorizar. O verão representa o estado mais saudável, mas não se esqueça que mesmo o inverno faz parte do ciclo natural da vida!"
         page5.startButton.layer.cornerRadius = 10
         page5.walkthroughDelegate = self
         
